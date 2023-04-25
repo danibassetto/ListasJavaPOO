059020fc86295386a4206e6b54daaa8ce9d6af27
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-04-25 09:27:18 -0300

    new

--- Lista06/src/AtividadeHeranca.java
+++ Lista06/src/AtividadeHeranca.java
@@ -25,8 +25,9 @@ class Fornecedor
 
 class Produto
 {
-    private int codigo, qtdeVendida;
+    private int codigo;
     private String descricao;
+    protected int qtdeVendida;
     private float preco;
     private Fornecedor fornecedor;
 
@@ -70,9 +71,9 @@ class Peca extends Produto
     {
         if (qtde <= this.qtdeDisponivel)
         {
-           this.qtdeDisponivel -= qtde;
-           //set qtdeVendida
-           return true;
+            this.qtdeDisponivel -= qtde;
+            this.qtdeVendida += qtde;
+            return true;
         }
         else return false;
     }
@@ -94,7 +95,7 @@ class Servico extends Produto
 
     public boolean vender(int qtde)
     {
-        //set qtdeVendida
+        this.qtdeVendida += qtde;
         return true;
     }
 }
@@ -107,7 +108,7 @@ class Principal
 
         System.out.println("Informe os dados do 1º Fornecedor: ");
         System.out.println("Nome: ");
-        String nome1 = teclado.nextLine();
+        String nome1 = teclado.nextLine().toUpperCase();
         System.out.println("Endereço: ");
         String endereco1 = teclado.nextLine();
         System.out.println("Telefone: ");
@@ -117,7 +118,7 @@ class Principal
 
         System.out.println("\nInforme os dados do 2º Fornecedor: ");
         System.out.println("Nome: ");
-        String nome2 = teclado.nextLine();
+        String nome2 = teclado.nextLine().toUpperCase();
         System.out.println("Endereço: ");
         String endereco2 = teclado.nextLine();
         System.out.println("Telefone: ");
@@ -125,21 +126,25 @@ class Principal
 
         Fornecedor fornecedor2 = new Fornecedor(nome2, endereco2, telefone2);
 
-        System.out.println("Informe os dados da peça: ");
+        System.out.println("\nFornecedores cadastrados com sucesso!");
+
+        System.out.println("\nInforme os dados da peça: ");
         System.out.println("Código: ");
         int codigoPeca = teclado.nextInt();
+        teclado.nextLine();
         System.out.println("Descrição: ");
-        String descricaoPeca = teclado.nextLine();
+        String descricaoPeca = teclado.nextLine().toUpperCase();
         System.out.println("Preço: ");
         float precoPeca = teclado.nextFloat();
 
         Peca peca = new Peca(codigoPeca, descricaoPeca, precoPeca, fornecedor1);
 
-        System.out.println("Informe os dados do serviço: ");
+        System.out.println("\nInforme os dados do serviço: ");
         System.out.println("Código: ");
         int codigoServico = teclado.nextInt();
+        teclado.nextLine();
         System.out.println("Descrição: ");
-        String descricaoServico = teclado.nextLine();
+        String descricaoServico = teclado.nextLine().toUpperCase();
         System.out.println("Preço: ");
         float precoServico = teclado.nextFloat();
         System.out.println("Tempo de serviço: ");
@@ -147,6 +152,22 @@ class Principal
 
         Servico servico = new Servico(codigoServico, descricaoServico, precoServico, fornecedor2, tempoServico);
 
-        
+        System.out.println("\nQuanto você deseja repor da peça " + peca.getDescricao() + "?");
+        peca.repor(teclado.nextInt());
+
+        System.out.println("\nQuanto deseja vender da peça/produto " + peca.getDescricao() + "?");
+        if(peca.vender(teclado.nextInt()))
+            System.out.println("Venda realizada com sucesso!");
+        else System.out.println("Venda não realizada, quantidade indisponível!");
+
+        System.out.println("\nQuanto deseja vender do serviço/produto " + servico.getDescricao() + "?");
+        servico.vender(teclado.nextInt());
+        System.out.println("Venda realizada com sucesso!");
+
+        Produto produtoComMaiorFaturamento = peca.getFaturamento() > servico.getFaturamento() ? peca : servico;
+
+        System.out.println("\nFornecedor com maior faturamento: " + produtoComMaiorFaturamento.getFornecedor().getNome()
+                + "\nProduto vendido: " + produtoComMaiorFaturamento.getDescricao()
+                + "\nValor vendido: R$" + String.format("%.2f", produtoComMaiorFaturamento.getFaturamento()));
     }
 }
